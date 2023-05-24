14811347eaa417dc63a1d97957b0e800c673cffa
tahaakgungor <tahaakgungor@gmail.com>
2023-05-24 15:38:10 +0300

    ad

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:27017`, {
+    console.log("Connecting to MongoDB...", process.env.DB_USER, process.env.DB_PASSWORD);
+    await mongoose.connect(`mongodb://tahaakgungor:36987412tT@host.docker.internal:27015`), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+    };
 
-    });
     console.log("MongoDB connected...");
   } catch (error) {
     console.error(error.message);
